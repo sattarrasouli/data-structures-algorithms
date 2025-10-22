45e82d77deaf9e4346a15a826a2168c3101a170b
sattar <rasoulisattar@gmail.com>
2025-10-22 22:49:06 +0330

    add: insertion sort

---
+++ src/typescript/algorithms/sorting-and-search/insertion-sort/insertionSort.ts
@@ -0,0 +1,14 @@
+function insertionSort(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    const key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+console.log(insertionSort([12, 11, 13, 5, 6])); // ➜ [5, 6, 11, 12, 13]
